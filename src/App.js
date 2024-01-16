9c9b72fda974661f512c2dc2b6745f7e57704e08
MohcineSafi <safi.mouhcine02@gmail.com>
2024-01-17 00:10:40 +0100

    Add React Manage Interns Code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Stagiaire from "./components/stagiaire/Stagiaire";
+import ListStagiaire from "./components/stagiaire/ListStagiaire";
+
+function App() {
+  return (
+    <div className="App">
+        <ListStagiaire/>
+    </div>
+  );
+}
+
+export default App;
